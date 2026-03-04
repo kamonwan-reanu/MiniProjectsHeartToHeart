571e4c62e78e9e1130b1f924c530e2335ea4f9e3
napat68 <aoridea13@gmail.com>
2026-03-05 04:27:56 +0700

    Test

--- model/StoryIndex.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class StoryIndex {
-    // เริ่มต้น Single จริง
-    public static final String SINGLE_START = "SCENE_1";
-
-    // จุดเทสยอดฮิต (เลือกตามที่คุณใช้บ่อย)
-    public static final String TEST_SCENE = "SCENE_1"; // เปลี่ยนเป็น SCENE_6 ได้ตอนเทส HUB
-}
